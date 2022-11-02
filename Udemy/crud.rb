59e2d2477382fe17a4724179f878dd692a3f4263
Molly Risley <mollyarisley@gmail.com>
2022-11-02 14:48:59 -0500

    authenication, bcrypt, classes example

--- Udemy/crud.rb
+++ Udemy/crud.rb
@@ -23,4 +23,16 @@ def create_secure_users(list_of_users)
   end
 end
 
-create_secure_users(users)
+new_users = create_secure_users(users)
+puts new_users
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+      return "Authentication passed #{user_record}"
+    end 
+  end
+  "Credentials were not correct"
+end
+
+p authenticate_user("heisenberg", "password5", new_users)
